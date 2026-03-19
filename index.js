af59884eb80e44dfa3260f9c0adeea28fc50baef
Prashanth <prashanthreddy@Prashanths-MacBook-Air.local>
2026-03-19 18:34:19 -0500

    Fix dotenv for Railway

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const { createClient } = require("@supabase/supabase-js");
